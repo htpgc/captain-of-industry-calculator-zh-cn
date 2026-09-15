a3943cabe00fc2ab54f8b435804f4576c0179c5d
htpgc <64995996+htpgc@users.noreply.github.com>
2026-09-15 17:20:29 +0800

    chore: clear repository before source replacement

--- site/types/rollup-plugin-systemjs-loader.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'rollup-plugin-systemjs-loader' {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    export default function systemJSLoader(options: any): any;
-}
